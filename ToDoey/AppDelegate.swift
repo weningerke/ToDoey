4023177115194f667eb8d81043b8c2a9ef113241
weningerke <kovacsmi94@gmail.com>
2018-10-01 18:28:04 +0200

    Data persisted UserDefaults method

--- ToDoey/AppDelegate.swift
+++ ToDoey/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+//  Első sor ami elindul a programban
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        
         return true
     }
 
--- ToDoey/TodoListViewController.swift
+++ ToDoey/TodoListViewController.swift
@@ -12,9 +12,14 @@ class TodoListViewController: UITableViewController {
 
     
     var itemArray = ["vmi"]
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
 
     }
     
@@ -51,7 +56,7 @@ class TodoListViewController: UITableViewController {
        
     }
 
-        //MARK - Add New Item
+        // Mark - Add New Item
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -62,9 +67,14 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user click the Add Item button our UIAlert
             //print(textField.text)
-            self.itemArray.append(textField.text!)
             
-            self.tableView.reloadData()        }
+            
+            self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+            self.tableView.reloadData()
+            
+        }
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
